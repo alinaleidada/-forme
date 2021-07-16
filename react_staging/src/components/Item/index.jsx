ec82f8e185fc20b9da170d44ab9efc43c65277f0
leidan <18382912026>
2021-07-16 13:34:21 +0800

    按钮每个item背景色动态修改

--- react_staging/src/components/Item/index.jsx
+++ react_staging/src/components/Item/index.jsx
@@ -2,22 +2,30 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+	state={changecolor:false}
 	handleCheck=(id)=>{
 		return (event)=>{
 			this.props.updateTodo(id,event.target.checked)
 		}
 	}
 	handleDelete=(id)=>{
+		
 		return()=>{
 			if(window.confirm('确定删除吗？')){
 				this.props.deleteTodo(id)
 			}
 		}
 	}
+	changeBg=(id)=>{
+		return()=>{
+			this.setState({changecolor:id})
+		}
+	}
 	render() {
+		const {changecolor} = this.state;
 		const {name,done,id} = this.props
 		return (
-			<li>
+			<li style={{backgroundColor:changecolor?'#ccc':'#fff'}} onMouseEnter={this.changeBg(true)} onMouseLeave={this.changeBg(false)}>
 				<label>
 				<input type="checkbox" checked={done} onChange={this.handleCheck(id)}/>
 					<span>{name}</span>
